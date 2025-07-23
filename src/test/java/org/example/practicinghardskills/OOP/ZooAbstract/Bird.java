603ddbf5fb947c0e9334de8781fb99653730d63c
artem <artemminsadyrov@gmail.com>
2025-07-23 10:39:52 +0200

    moved files to ZooAbstract

--- src/test/java/org/example/practicinghardskills/OOP/ZooAbstract/Bird.java
+++ src/test/java/org/example/practicinghardskills/OOP/ZooAbstract/Bird.java
@@ -1,4 +1,4 @@
-package org.example.practicinghardskills.OOP.Zoo;
+package org.example.practicinghardskills.OOP.ZooAbstract;
 
 public class Bird extends Animal {
 
--- src/test/java/org/example/practicinghardskills/OOP/ZooAbstract/main.java
+++ src/test/java/org/example/practicinghardskills/OOP/ZooAbstract/main.java
@@ -1,4 +1,4 @@
-package org.example.practicinghardskills.OOP.Zoo;
+package org.example.practicinghardskills.OOP.ZooAbstract;
 
 public class main {
     public static void main(String[] args) {
@@ -8,5 +8,6 @@ public class main {
         zoo.addAnimal(elephant);
         zoo.addAnimal(bird);
         zoo.showAll();
+
     }
 }
