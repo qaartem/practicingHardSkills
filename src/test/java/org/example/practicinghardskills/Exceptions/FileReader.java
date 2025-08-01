4e4f7fdba8e52952baae204c1828a9f98632eadf
artem <artem.mindsadyrov@actumdigital.com>
2025-08-02 01:47:25 +0200

    rename

--- src/test/java/org/example/practicinghardskills/Exceptions/FileReader.java
+++ src/test/java/org/example/practicinghardskills/Exceptions/FileReader.java
@@ -1,13 +1,12 @@
 package org.example.practicinghardskills.Exceptions;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 
-public class FileReaderTask {
+public class FileReader {
 
     public static void main(String[] args) {
         try {
-            FileReader reader = new FileReader("data.txt");
+            java.io.FileReader reader = new java.io.FileReader("data.txt");
             System.out.println("Файл успешно открыт.");
             System.out.println("Текущая директория: " + System.getProperty("user.dir"));
 
