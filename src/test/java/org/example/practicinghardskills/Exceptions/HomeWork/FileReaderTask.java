d9cc1e1aef19634df22551b33058a0661daccf78
artem <artem.mindsadyrov@actumdigital.com>
2025-08-02 01:47:36 +0200

    add try-with-resources

--- src/test/java/org/example/practicinghardskills/Exceptions/HomeWork/FileReaderTask.java
+++ src/test/java/org/example/practicinghardskills/Exceptions/HomeWork/FileReaderTask.java
@@ -2,14 +2,18 @@ package org.example.practicinghardskills.Exceptions.HomeWork;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileReaderTask {
     public static void main(String[] args) {
-        try {
-            FileReader reader = new FileReader("data.txt");
+        try (FileReader reader = new FileReader("data.txt")) {
             System.out.println("File is opened");
+            // Здесь можно читать содержимое файла, если нужно
         } catch (FileNotFoundException e) {
-            System.out.println("File is not opened");
+            System.out.println("File is not opened - not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("File could not be read");
             e.printStackTrace();
         }
         System.out.println("Program is working");
