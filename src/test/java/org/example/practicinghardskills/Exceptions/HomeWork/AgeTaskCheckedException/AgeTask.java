a9dbea9a2f9242e482a6e544a29062b22650531b
artem <artem.mindsadyrov@actumdigital.com>
2025-08-02 12:40:46 +0200

    add The condition makes it clear that 0 and 150 are included in the valid range

--- src/test/java/org/example/practicinghardskills/Exceptions/HomeWork/AgeTaskCheckedException/AgeTask.java
+++ src/test/java/org/example/practicinghardskills/Exceptions/HomeWork/AgeTaskCheckedException/AgeTask.java
@@ -4,7 +4,7 @@ package org.example.practicinghardskills.Exceptions.HomeWork.AgeTaskCheckedExcep
 public class AgeTask {
     public static void main(String[] args) {
         try {
-            ageValidation(50);
+            ageValidation(149);
         } catch (InvalidAgeException e) {
             System.out.println(e.getMessage());
         }
@@ -13,11 +13,12 @@ public class AgeTask {
     // Valid human age is expected to be in range 0–150.
     // This check prevents unexpected input like -5 or 999.
     public static void ageValidation(int age) throws InvalidAgeException {
-        if (age < 0 || age > 150) {
-            throw new InvalidAgeException("Age should be from 0 to 150");
+        if (age >= 0 && age <= 150) {
+            System.out.println(age);
+        } else {
+            throw new InvalidAgeException("Age should be between 0 and 150 (inclusive)");
         }
 
-        System.out.println(age);
     }
 
 }
